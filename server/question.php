035d07385365a58a2388a507d249807dec1d01df
Salih Salih <salihelobeid@gmail.com>
2022-03-16 12:52:54 -0400

    changed question according to the last meeting and updated readme

--- server/question.php
+++ server/question.php
@@ -1,68 +1,69 @@
 <?php include("function.php"); ?>
 <div class="controller">
 	<div class="title">
-		<h2><?php echo ($_REQUEST['i'] == 'addsurvey' ? 'Add a new Survey' : 'View Surveys'); ?></h2>
+		<h2><?php echo ($_REQUEST['i'] == 'addquestion' ? 'Add a new Question' : 'View Questions'); ?></h2>
 	</div>
 	<div class="form">
-		<?php if ($_REQUEST['i'] == 'addsurvey') { ?>
+		<?php if ($_REQUEST['i'] == 'addquestion') { ?>
 
-			<form action="action.php?y=addsurvey" method="post">`
-				<!-- Start Survey Class -->
+			<form action="action.php?y=addquestion" method="post">`
+				<!-- Start Question Class -->
 				<div class="questinInfo">
 					
-					<!-- Start Survey Title -->
+					<!-- Start Question Title -->
 					<div class="qTitle qst qMargin">
-						<label for="qTitle" class="qst lbl">Survey <span class="qi"></span>&nbsp;Title :</label>
+						<label for="qTitle" class="qst lbl">Question <span class="qi"></span>&nbsp;Title :</label>
 						<input type="text" id="qTitle" class="inputFlex" name="qTitle">
 					</div>
-					<!-- End Survey Title -->
+					<!-- End Question Title -->
 						
-					<!-- Start Survey Form -->
+					<!-- Start Question Form -->
 					<div class="question qst">
 
-						<!-- Start Survey Number -->
+						<!-- Start Question Number -->
 						<div class="qst qMargin">
 							<div class="qNum">
 								<h2>Question <span class="qi">&nbsp;#</span></h2>
 							</div>
 						</div>
-						<!-- End Survey Number -->
+						<!-- End Question Number -->
 
-						<!-- Start Survey Type -->
+						<!-- Start Question Type -->
 						<div class="qst qMargin">
 							<label for="qType" class="qst lbl">Question Type</label>
 							<select name="qType" id="qType" class="qType inputFlex">
-								<option selected="true" disabled="disabled">Survey Type</option>
+								<option selected="true" disabled="disabled">Question Type</option>
 								<option value="shortAnswer">Short Answer</option>
 								<option value="checkBox" selected="selected">CheckBox</option>
 								<option value="radio">Radio</option>
 							</select>
 						</div>
-						<!-- End Survey Type -->
+						<!-- End Question Type -->
 
-						<!-- Start Survey Text -->
+						<!-- Start Question Text -->
 						<div class="qText qst qMargin">
 							<label for="qText" class="qst lbl">Question Text</label>
 							<input type="text" id="qText" name="qText" class="inputFlex">
 						</div>
-						<!-- End Survey Text -->
+						<!-- End Question Text -->
 
-						<!-- Start Survey Choice Div -->
+						<!-- Start Question Choice Div -->
 						<div class="choiceSection qst qMargin">
 
-							<!-- Start Survey Choices Title -->
+							<!-- Start Question Choices Title -->
 							<div class="qChoices qMargin">
 								<h4>Choices</h4>
 							</div>
-							<!-- End Survey Choices Title -->
+							<!-- End Question Choices Title -->
 
-							<!-- Start Survey Choice Buttons -->
+							<!-- Start Question Choice Buttons -->
 							<div class="childChoice qst qMargin">
-								<label for="qText" class="qst lbl">Choice <span class="i">&nbsp;1</span>:</label>
+								<label for="qText" class="qst lbl">Choice <span class="i">&nbsp;#</span>:</label>
 								<input type="text" id="input" name="qText" class="inputFlex" />
+								<input class="addBranchQuestion qst" type="button" value="Add branch question">
 								<input class="choiceButtonDelete qst" type="button" value="Delete Choice">
 							</div>
-							<!-- End Survey Choice Buttons -->
+							<!-- End Question Choice Buttons -->
 
 							<!-- Start Add Another Choice Div -->
 							<div class="choiceButtonGroup qst"></div>
@@ -75,36 +76,29 @@
 							<!-- End Add Choice Button -->
 
 						</div>
-						<!-- End Survey Choice Div -->
-
-						<!-- Start Add New + Delete Survey -->
-						<div class="newQButton qst">
-							<input type="button" class="addQ" title="Add Question" value="Add Question">
-							<input type="button" class="deleteQ" value="Delete Question">
-						</div>
-						<!-- End Add New + Delete Survey -->
+						<!-- End Question Choice Div -->
 
 					</div>
-					<!-- End Survey Form -->
+					<!-- End Question Form -->
 
-					<!-- Start New Survey Div -->
+					<!-- Start New Question Div -->
 					<div class="anotherQ"></div>
-					<!-- End New Survey Div -->
+					<!-- End New Question Div -->
 
-					<!-- Start Save + delete Survey Buttons -->
+					<!-- Start Save + delete Question Buttons -->
 					<div class="qst rightSubmit">
-						<input type="button" value="Delete Survey">
+						<input type="button" value="Delete Question">
 						<input type="submit" value="Save">
 					</div>
-					<!-- End Save + delete Survey Buttons -->
+					<!-- End Save + delete Question Buttons -->
 
 				</div>
-				<!-- End Survey Class -->
+				<!-- End Question Class -->
 
 			</form>
 
 		<?php }
-		if ($_REQUEST['i'] == 'viewsurvey') { ?>
+		if ($_REQUEST['i'] == 'viewquestion') { ?>
 
 
 		<?php } ?>
